d78845fd1e1cb7d045392d2df98180b25ac623c3
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2025-04-23 20:39:00 +0530

    added features

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import "./App.css";
+import Contact from "./sections/Contact/Contact";
+import Footer from "./sections/Footer/Footer";
+import Hero from "./sections/Hero/Hero";
+import Projects from "./sections/Projects/Projects";
+import Skills from "./sections/Skills/Skills";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
 
 export default App;
